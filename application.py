0f90eac204f0790eb8d18bdc8be024387514c024
nikolayotmakhov <nikolayotmakhov@outlook.com>
2020-11-17 01:36:48 +0200

    test for stock

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from assets import sql_login
 from logic import sql_functions
+import requests
 
 Running_local = False
 
@@ -29,12 +30,25 @@ def stocks():
 
 @app.route('/stockshow', methods=['GET','POST'])
 def stocksshow():
-  return render_template('stockinfo.html', stock_ticker=request.args.get("stock_ticker", "world"))
+    return render_template('stockinfo.html', stock_ticker = request.args.get("stock_ticker", "world"))
+
+def tester():
+    headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Token 33fa689353e4b7f1deec77ae17eeda1dbb432df6'
+    }
+    url = "https://api.tiingo.com/tiingo/daily/aapl"
+    try:
+        response = requests.get(url, headers=headers)
+        return "passed"
+    except:
+        return "failed"
 
 
 @app.route('/testsite' , methods=['GET','POST'])
 def testsite():
-    return render_template('testsite.html')
+    stock_ticker = tester()
+    return render_template('stockinfo.html',stock_ticker=stock_ticker)
 
 if __name__ == '__main__':
     Running_local = True
